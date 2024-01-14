f9c16dc2d229e17dee896668bd39bbd50da3a2dc
CapybaraWithANuke <126101517+CapybaraWithANuke@users.noreply.github.com>
2024-01-14 18:57:25 +0100

    backpack api finally working

--- src/components/Backpack.vue
+++ src/components/Backpack.vue
@@ -18,12 +18,15 @@ export default {
       turn: 0,
       count: 0,
       chosenButton: "",
+      tableData: [],
+      equippedAttacks: [],
+      equippedAttackNames: []
     }
   },
 
   mounted() {
     this.fetchPlayerAttacks();
-    this.unequipAll();
+    this.fetchEquippedAttacks();
   },
 
   methods: {
@@ -31,41 +34,76 @@ export default {
     async fetchPlayerAttacks() {
         try {
           // Get the bearer token from local storage
+            const bearerToken = localStorage.getItem('authToken');
+
+            const response = await fetch('https://balandrau.salle.url.edu/i3/players/attacks', {
+              method: 'GET',
+              headers: {
+                'accept': 'application/json',
+                'Bearer': `${bearerToken}`, 
+              },
+            });
+
+            const data = await response.json();
+
+            this.tableData = data.map((attack) => ({
+                attack_ID: attack.attack_ID,
+                positions: attack.positions,
+                power: attack.power,
+                price: attack.price,
+                equipped: attack.equipped
+            }));
+        } catch (error) {
+          console.error('Error fetching player attacks: ', error);
+        }
+    },
+
+    async fetchEquippedAttacks() {
+        try {
+          // Get the bearer token from local storage
           const bearerToken = localStorage.getItem('authToken');
 
           const response = await fetch('https://balandrau.salle.url.edu/i3/players/attacks', {
-          method: 'GET',
-          headers: {
-            'accept': 'application/json',
-            'Bearer': `${bearerToken}`, 
-          },
-        });
+              method: 'GET',
+              headers: {
+                'accept': 'application/json',
+                'Bearer': `${bearerToken}`, 
+              },
+          });
 
-        const data = await response.json();
+          const data = await response.json();
 
-        this.tableData = data.map((attack) => ({
+          this.equippedAttacks = data.map((attack) => ({
             attack_ID: attack.attack_ID,
-            positions: attack.positions,
-            power: attack.power,
-            price: attack.price
-          }));
+            equipped: attack.equipped
+          })).filter((attack) => attack.equipped === true);
+
+          while (this.equippedAttacks.length < 3) {
+              let emptyAttack = {'attack_ID': "", positions: (0,0), power: 0, price: 0, equipped: false};
+              this.equippedAttacks.push(emptyAttack);
+          }
+
+          for (let i = 0; i < 3; ++i) {
+            this.equippedAttackNames.push(this.equippedAttacks.at(i).attack_ID);
+          }
+
         } catch (error) {
-          console.error('Error fetching player data: ', error);
+          console.error('Error fetching player attacks: ', error);
         }
     },
 
     unequipAttack(attack) {
         try {
           // Get the bearer token from local storage
-          const bearerToken = localStorage.getItem('authToken');
+            const bearerToken = localStorage.getItem('authToken');
 
-          fetch('https://balandrau.salle.url.edu/i3/players/attacks/' + attack, {
-          method: 'DELETE',
-          headers: {
-            'accept': 'application/json',
-            'Bearer': `${bearerToken}`, 
-          },
-        });
+            fetch('https://balandrau.salle.url.edu/i3/players/attacks/' + attack, {
+               method: 'DELETE',
+               headers: {
+                 'accept': 'application/json',
+                 'Bearer': `${bearerToken}`, 
+                },
+            });
         } catch (error) {
           console.error('Error unequipping attack: ', error);
         }
@@ -107,14 +145,14 @@ export default {
           const bearerToken = localStorage.getItem('authToken');
 
           const response = await fetch('https://balandrau.salle.url.edu/i3/players/attacks/' + newAttack + '/' + attackToChange, {
-          method: 'PATCH',
-          headers: {
-            'accept': 'application/json',
-            'Bearer': `${bearerToken}`, 
-          },
-        });
+              method: 'PATCH',
+              headers: {
+                'accept': 'application/json',
+                'Bearer': `${bearerToken}`, 
+              },
+          });
 
-        if (response.ok) {
+          if (response.ok) {
                 if (response.status == 204) {
                   return true; 
                 } else {
@@ -123,65 +161,41 @@ export default {
             } else {
                 return false;
             }
-        } catch (error) {
+      } catch (error) {
           console.error('Error substituting attack: ', error);
-        }
+      }
     },
 
     async selectAbility(name) {
 
       if (this.chosenButton != "") {
-        if (document.getElementById(this.chosenButton).textContent != "") {
-          if (await this.substituteAttack(document.getElementById(this.chosenButton).textContent, name)) {
-            document.getElementById(this.chosenButton).innerHTML = name;
-          }
-        }
-        else {
-          if (await this.equipAttack(name)) {
-            document.getElementById(this.chosenButton).innerHTML = name;
+          if (document.getElementById(this.chosenButton).textContent != "") {
+              if (await this.substituteAttack(document.getElementById(this.chosenButton).textContent, name)) {
+                document.getElementById(this.chosenButton).innerHTML = name;
+              }
           }
           else {
-            console.log("OwO");
+              if (await this.equipAttack(name)) {
+                document.getElementById(this.chosenButton).innerHTML = name;
+              }
           }
-        }
 
       }
-    },
-
-    unequipAll() {
-
-      const bearerToken = localStorage.getItem('authToken');
-
-      fetch('https://balandrau.salle.url.edu/i3/players/attacks/20twentyletterword20', {
-        method: 'DELETE',
-        headers: {
-          'accept': 'application/json',
-          'Bearer': `${bearerToken}`, 
-        },
-      });
-
-      fetch('https://balandrau.salle.url.edu/i3/players/attacks/kazakh%20devastation', {
-        method: 'DELETE',
-        headers: {
-          'accept': 'application/json',
-          'Bearer': `${bearerToken}`, 
-        },
-      });
     },
 
     buttonPressed(btn) {
 
-      if (getComputedStyle(document.getElementById(btn))['backgroundColor'] == "rgb(235, 0, 0)") {
-        if (document.getElementById(btn).textContent != "") {
-          this.unequipAttack(document.getElementById(btn).textContent);
-          document.getElementById(this.chosenButton).innerHTML = "";
+        if (getComputedStyle(document.getElementById(btn))['backgroundColor'] == "rgb(235, 0, 0)") {
+            if (document.getElementById(btn).textContent != "") {
+                this.unequipAttack(document.getElementById(btn).textContent); 
+                document.getElementById(this.chosenButton).innerHTML = "";
+            }
         }
-      }
-      document.getElementById('button1').style.backgroundColor = "#FFFFFF";
-      document.getElementById('button2').style.backgroundColor = "#FFFFFF";
-      document.getElementById('button3').style.backgroundColor = "#FFFFFF";
-      document.getElementById(btn).style.backgroundColor = "#EB0000";
-      this.chosenButton = btn;
+        document.getElementById('button1').style.backgroundColor = "#FFFFFF";
+        document.getElementById('button2').style.backgroundColor = "#FFFFFF";
+        document.getElementById('button3').style.backgroundColor = "#FFFFFF";
+        document.getElementById(btn).style.backgroundColor = "#EB0000";
+        this.chosenButton = btn;
 
 
     }
@@ -191,19 +205,20 @@ export default {
 </script>
 
 <template>
-      <button @click="toggleMenu" class="home_logo"><img src="../assets/images/HomeLogo.png" alt="Home Logo"></button>
+      <nav>
+        <button @click="toggleMenu" class="home_logo"><img src="../assets/images/HomeLogo.png" alt="Home Logo"></button>
 
-      <MenuMV :isVisible="isMenuVisible" @toggleMenu="toggleMenu" />
+        <MenuMV :isVisible="isMenuVisible" @toggleMenu="toggleMenu" />
+      </nav>
       <div class="form_container">
         <div class="tab_box">
           <label class="backpack-text"><strong>BACKPACK</strong></label>
         </div>
 
         <div class="content_box">
-          <button class="btn-abilities signup_button" id="button1" @click="buttonPressed('button1')"></button><br>
-          <button class="btn-abilities signup_button" id="button2" @click="buttonPressed('button2')"></button><br>
-          <button class="btn-abilities signup_button" id="button3" @click="buttonPressed('button3')"></button>
-
+          <button class="btn-abilities signup_button" id="button1" @click="buttonPressed('button1')">{{ this.equippedAttackNames.at(0) }}</button><br>
+          <button class="btn-abilities signup_button" id="button2" @click="buttonPressed('button2')">{{ this.equippedAttackNames.at(1) }}</button><br>
+          <button class="btn-abilities signup_button" id="button3" @click="buttonPressed('button3')">{{ this.equippedAttackNames.at(2) }}</button>
 
           <table class="table-container">
             <thead class="table-header">
@@ -230,26 +245,12 @@ export default {
 
 <style scoped>
 
-/* .Screen {
-    width: 800px;
-    height: 1280px;
-    background: url("../assets/images/BackgroundBackpackMV.png");
-    margin: 0px;
-    position: absolute;
-    border: 1px solid rgba(0,0,0,1);
-} */
+
 .form_container {
   padding: 0px;
   width: 90%;
   margin-top: 257px;
 } 
-
-/* @media screen and (min-width: 1000px) {
-  .form_container {
-    width: 2000px;
-    height: auto;
-  }
-} */
 
 .table-container {
   min-width: 300px;
@@ -279,82 +280,9 @@ th {
 }
 
 .btn-abilities {
-  min-height: 95px;
-  min-width: 500px; 
+  height: 90px;
+  width: 500px; 
   color: black;
 }
-/*
-td{
-  width: 174px;
-  padding: 10px;
-}
 
-td:hover{
-  cursor: default;
-}
-
-.table-abilities{
-  position: absolute;
-  top: 440px;
-  color: black;
-  left: 300px;
-}
-
-.td-abilities{
-  padding: 8.5px;
-} */
-/*
-@media screen and (min-width: 2421px) {
-  .Screen {
-    width: 1728px;
-    height: 1117px;
-    background: url("../assets/images/BACKPACK.png");
-  }
-
-  .backpack-text {
-    font-size: 60px;
-    top: 140px;
-    left: 675px;
-  }
-
-  .name-text {
-    font-size: 40px;
-    top: 350px;
-    left: 750px;
-  }
-
-  .power-text {
-    font-size: 40px;
-    top: 350px;
-    left: 980px;
-  }
-
-  .level-text {
-    font-size: 40px;
-    top: 350px;
-    left: 1175px;
-  }
-
-  .table-cell {
-    font-size: 25px;
-    height: 85px;
-  }
-
-  .column-name {
-    top: 445px;
-    left: 755px;    
-  }
-
-  .column-power {
-    top: 445px;
-    left: 1040px;    
-  }
-
-  .column-level {
-    top: 445px;
-    left: 1290px;    
-  }
-  
-}
-*/
 </style>
